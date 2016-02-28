15fe9cff524d79ab7958c0c4cea9921368bfc841
wonghoifung <wonghoifung85@gmail.com>
2016-02-28 08:48:55 +0800

    server

--- netty/nettybaby/src/main/java/com/wong/TimeServer.java
+++ netty/nettybaby/src/main/java/com/wong/TimeServer.java
@@ -30,7 +30,7 @@ public class TimeServer
 			workerGroup.shutdownGracefully();
 		}
 	}
-	private static class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+	private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
 		@Override
 		protected void initChannel(SocketChannel arg0) throws Exception {
 			arg0.pipeline().addLast(new TimeServerHandler());
@@ -47,29 +47,5 @@ public class TimeServer
         	}
         }
         new TimeServer().bind(port);
-    }
-    public static class TimeServerHandler extends ChannelHandlerAdapter {
-    	@Override
-    	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-    		ByteBuf buf = (ByteBuf)msg;
-    		byte[] req = new byte[buf.readableBytes()];
-    		buf.readBytes(req);
-    		String body = new String(req, "UTF-8");
-    		System.out.println(body);
-    		String currentTime = "bad";
-    		if ("time".equalsIgnoreCase(body)) {
-    			currentTime = new java.util.Date(System.currentTimeMillis()).toString();
-    		}
-    		ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
-    		ctx.write(resp);
-    	}
-    	@Override
-    	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-    		ctx.flush();
-    	}
-    	@Override
-    	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-    		ctx.close();
-    	}
     }
 }
---
+++ netty/nettybaby/src/main/java/com/wong/TimeServerHandler.java
@@ -0,0 +1,38 @@
+package com.wong;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.nio.NioServerSocketChannel; 
+import io.netty.channel.EventLoopGroup;
+import io.netty.bootstrap.ServerBootstrap; 
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ByteBuf buf = (ByteBuf)msg;
+		byte[] req = new byte[buf.readableBytes()];
+		buf.readBytes(req);
+		String body = new String(req, "UTF-8");
+		System.out.println(body);
+		String currentTime = "bad";
+		if ("time".equalsIgnoreCase(body)) {
+			currentTime = new java.util.Date(System.currentTimeMillis()).toString();
+		}
+		ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+		ctx.write(resp);
+	}
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ctx.flush();
+	}
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		ctx.close();
+	}
+}
