e5d6c2506b85e6cc0c251130203fb3f86852ea59
wonghoifung <wonghoifung85@gmail.com>
2017-05-28 20:33:46 +0800

    .

---
+++ java8/ch2.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class ch2
+{
+	public static void main(String[] args)
+	{
+		List<Apple> inventory = Arrays.asList(new Apple(80,"green"), new Apple(155, "green"), new Apple(120, "red"));
+		List<Apple> greenApples2 = filter(inventory, new AppleColorPredicate());
+		System.out.println(greenApples2);
+	}
+	public static List<Apple> filter(List<Apple> inventory, ApplePredicate p){
+		List<Apple> result = new ArrayList<>();
+		for(Apple apple : inventory){
+			if(p.test(apple)){
+				result.add(apple);
+			}
+		}
+		return result;
+	} 
+	public static class Apple {
+		private int weight = 0;
+		private String color = "";
+
+		public Apple(int weight, String color){
+			this.weight = weight;
+			this.color = color;
+		}
+
+		public Integer getWeight() {
+			return weight;
+		}
+
+		public void setWeight(Integer weight) {
+			this.weight = weight;
+		}
+
+		public String getColor() {
+			return color;
+		}
+
+		public void setColor(String color) {
+			this.color = color;
+		}
+
+		public String toString() {
+			return "Apple{" +
+					"color='" + color + '\'' +
+					", weight=" + weight +
+					'}';
+		}
+	}
+	interface ApplePredicate {
+		public boolean test(Apple a);
+	}
+	static class AppleWeightPredicate implements ApplePredicate{
+		public boolean test(Apple apple){
+			return apple.getWeight() > 150; 
+		}
+	}
+	static class AppleColorPredicate implements ApplePredicate{
+		public boolean test(Apple apple){
+			return "green".equals(apple.getColor());
+		}
+	}
+	static class AppleRedAndHeavyPredicate implements ApplePredicate{
+		public boolean test(Apple apple){
+			return "red".equals(apple.getColor()) 
+					&& apple.getWeight() > 150; 
+		}
+	}
+}
